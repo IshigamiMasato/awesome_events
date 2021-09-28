fcb925d9ee7b45ec4161dae0aa71630d631f5ef0
I.M <i.m@aiMnoMacBook-Air.local>
2021-09-28 20:14:07 +0900

    Implement retirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  before_destroy :check_all_events_finished
+
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントがあります"
+    end
+
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントがあります"
+    end
+  
+    throw(:abort) unless errors.empty?
+  end
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # 認証成功時に下期のURLにアクセスする
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :retirements
 end
